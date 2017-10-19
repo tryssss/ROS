d562d04a4ac24df973918d033eef153fad2a05d5
tryssss <try_ssss@163.com>
2017-10-19 10:39:09 +0000

    first commit

---
+++ catkin_ws/build/neginner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/czy/ROS/catkin_ws/src/neginner
+# Build directory: /root/czy/ROS/catkin_ws/build/neginner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
